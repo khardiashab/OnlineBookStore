331eb678bdd2c689116d5b6a03afc58ad4245f79
Ubuntu <ubuntu@ip-172-31-43-131.ec2.internal>
2024-10-09 09:16:33 +0000

    user icon url added in dummy data

--- dummy-backend/index.js
+++ dummy-backend/index.js
@@ -23,7 +23,7 @@ let users = [
     email: "john@example.com",
     provider: "local",
     oauthId: "",
-    picture: "",
+    picture: "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg",
   },
 ];
 
